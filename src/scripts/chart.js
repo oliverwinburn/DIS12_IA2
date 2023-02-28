3e14f83f37facf6d9946400e67477f76326960e0
oliverwinburn <owinburn15@outlook.com>
2023-02-28 20:21:24 +1000

    add humidity graph

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -3,51 +3,54 @@ import { Chart } from "chart.js";
 /** @type {HTMLSpanElement} */
 const tempDisplay = document.getElementById('temp-display')
 
-/** @type {HTMLCanvasElement} */
-const canvas = document.getElementById("chart")
-const ctx = canvas.getContext('2d')
+/** @type {CanvasRenderingContext2D} */
+const ctxTemp = document.getElementById("chartTemp").getContext("2d")
 
+/** @type {CanvasRenderingContext2D} */
+const ctxHumid = document.getElementById("chartHumid").getContext("2d")
 
-/**
- * @type {{x: number, y: number}[]}
- */
+/** @type {{x: number, y: number}[]} */
 let temperatureDataset = []
 
-const chart = new Chart(ctx, {
-    type: "line",
-    data: {
-        datasets: [{ label: "Temperature", data: temperatureDataset, tension: 0.3 }],
-    },
-    options: {
+/** @type {{x: number, y: number}[]} */
+let humidityDataset = []
+
+const options = (text) => {
+    return {
         scales: {
             x: {
                 type: 'linear',
                 position: 'bottom',
                 title: "Time",
-                time: {
-                    unit: "millisecond",
-                },
-                ticks: {
-                    display: false,
-                },
-                grid: {
-                    display: false,
-                },
+                time: { unit: "millisecond", },
+                ticks: { display: false, },
+                grid: { display: false, },
             },
             y: {
                 type: "linear",
                 position: "left",
-                title: {
-                    text: "Temperature (°C)",
-                    display: true,
-                    font: {
-                        size: 16
-                    }
-                },
+                title: { text, display: true, font: { size: 16 }, },
+                suggestedMax: 30,
+                suggestedMin: 20,
             }
-        },
-        animation: false
+        }, animation: false,
+    }
+}
+
+const chartTemp = new Chart(ctxTemp, {
+    type: "line",
+    data: {
+        datasets: [{ label: "Temperature", data: temperatureDataset, tension: 0.3, }],
     },
+    options: options("Temperature (°C)")
+})
+
+const chartHumid = new Chart(ctxHumid, {
+    type: "line",
+    data: {
+        datasets: [{ label: "Humidity", data: humidityDataset, tension: 0.3 }]
+    },
+    options: options("Humidity")
 })
 
 let latestTimestamp = 0
@@ -58,30 +61,40 @@ async function insertData() {
     /** @type {{id: number, timestamp: number, temperature: number, humidity: number}[]} */
     const data = JSON.parse((await fetch(`/api/fetch?timestamp=${latestTimestamp}`).then(r => r.text())))
     if (!data.length) return
-    data.sort((a, b) => b.timestamp - a.timestamp)
+    data.sort((a, b) => a.timestamp - b.timestamp)
 
-    latestTimestamp = data.at(0).timestamp
-    if (!initialTime) initialTime = data.at(-1).timestamp - 30000
-    // const filteredData = data.filter(d => latestTimestamp - d.timestamp > 20000)
+    latestTimestamp = data.at(-1).timestamp
+    if (!initialTime) initialTime = data.at(0).timestamp - 30000
 
-    const displayDataTemp = data.map(d => { return { x: d.timestamp - initialTime, y: d.temperature } })
-    displayDataTemp.sort((a, b) => a.x - b.x)
+    const tempData = data.map(d => { return { x: d.timestamp - initialTime, y: d.temperature } })
+    const humidData = data.map(d => { return { x: d.timestamp - initialTime, y: d.humidity } })
 
-    displayDataTemp.forEach(d => temperatureDataset.push(d))
-    const maxTemp = displayDataTemp.at(-1)
+    tempData.forEach(d => temperatureDataset.push(d))
+    humidData.forEach(d => humidityDataset.push(d))
+
+    const maxTemp = tempData.at(-1)
     const cutoffTime = maxTemp.x - 35000
 
-    let i = temperatureDataset.length
-    while (i--) {
-        if (temperatureDataset[i].x < cutoffTime) {
+    for (let i = 0; i < temperatureDataset.length; i++) {
+        if(temperatureDataset[i].x < cutoffTime) {
             temperatureDataset.splice(i, 1)
         }
     }
 
-    chart.options.scales.x.min = cutoffTime
-    chart.options.scales.x.max = maxTemp.x + 3000
+    for (let i = 0; i < humidityDataset.length; i++) {
+        if (humidityDataset[i].x < cutoffTime) {
+            humidityDataset.splice(i, 1)
+        }
+    }
 
-    chart.update()
+    chartTemp.options.scales.x.min = cutoffTime
+    chartHumid.options.scales.x.min = cutoffTime
+
+    chartTemp.options.scales.x.max = maxTemp.x + 3000
+    chartHumid.options.scales.x.max = maxTemp.x + 3000
+
+    chartTemp.update()
+    chartHumid.update()
 
     tempDisplay.innerText = `${maxTemp.y.toFixed(1)}°C`
     currentTemp = maxTemp.y
@@ -93,7 +106,7 @@ function updateAlarm() {
         if (currentTemp > insideThreshold) {
             tempDisplay.classList.add("alarm")
         } else tempDisplay.classList.remove("alarm")
-    } catch {}
+    } catch { }
 }
 
 const beginInserter = () => { insertData(); dataInserter = setInterval(insertData, 1500) }
